c8af572890ce068d279412f3b388a195ecb07a57
Arjun Patil <arjunshivaji100@gmail.com>
2021-04-24 21:17:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-Arjun:Arjun@43210@cluster0.shtoh.mongodb.net/blogDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("", {useNewUrlParser: true, useUnifiedTopology: true });
 
 const postSchema = new mongoose.Schema({
   title: String,
